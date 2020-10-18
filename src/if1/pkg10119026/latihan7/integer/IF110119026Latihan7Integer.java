bf8b18d135aec31224d4057a9120e9a91a3c0a6f
mkhatami34 <koabarab361@gmail.com>
2020-10-18 20:46:52 +0700

    initial commit

---
+++ src/if1/pkg10119026/latihan7/integer/IF110119026Latihan7Integer.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg10119026.latihan7.integer;
+
+/**
+ *
+ * @author Asus
+ * NAMA     : Muhammad Khatami
+ * KELAS    : PBO
+ * NIM      : 10119026
+ * Deskripsi Program : Program ini berisi program untuk menampilkan integer
+ */
+public class IF110119026Latihan7Integer {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        byte b;
+        short s;
+        int i;
+        long l;
+        b = 120;
+        s = 32767;
+        i = 1_000_000_000;
+        l = 90000000000L;
+        System.out.println("byte : " + b);
+        System.out.println("short : " + s);
+        System.out.println("int: " + i);
+        System.out.println("long : " + l);
+    }
+    
+}
